53bd02c67f014a4c1e9ebfbbf33469eb06b4c5a5
daniel.schroeder <daniel.dn.schroeder@deutschebahn.com>
2020-05-08 10:10:21 +0200

    make it prettier

--- index.ts
+++ index.ts
@@ -4,58 +4,62 @@ import https = require('https');
 import URL = require('url');
 
 /**
-* The event passed to the Lambda handler
-*/
+ * The event passed to the Lambda handler
+ */
 export interface Event {
     [key: string]: any;
 }
 
 /**
-* Function signature
-*/
+ * Function signature
+ */
 export type func = (input: any) => Promise<any>;
 
 /**
-* Custom CloudFormation resource helper
-*/
+ * Custom CloudFormation resource helper
+ */
 export class CustomResource {
-
     /**
-    * Stores functions executed when resource creation is requested
-    */
+     * Stores functions executed when resource creation is requested
+     */
     createFunctions: func[] = [];
 
     /**
-    * Stores functions executed when resource update is requested
-    */
+     * Stores functions executed when resource update is requested
+     */
     updateFunctions: func[] = [];
 
     /**
-    * Stores functions executed when resource deletion is requested
-    */
+     * Stores functions executed when resource deletion is requested
+     */
     deleteFunctions: func[] = [];
 
     /**
-    * The event passed to the Lambda handler
-    */
+     * The event passed to the Lambda handler
+     */
     event: Event;
 
     /**
-    * The context passed to the Lambda handler
-    */
+     * The context passed to the Lambda handler
+     */
     context: Context;
 
     /**
-    * The callback function passed to the Lambda handler
-    */
+     * The callback function passed to the Lambda handler
+     */
     callback: Callback;
 
     /**
-    * Logger class
-    */
+     * Logger class
+     */
     logger: Logger;
 
-    constructor(event: Event, context: Context, callback: Callback, logger?: Logger) {
+    constructor(
+        event: Event,
+        context: Context,
+        callback: Callback,
+        logger?: Logger
+    ) {
         if (typeof event.ResponseURL === 'undefined') {
             throw new Error('ResponseURL missing');
         }
@@ -68,41 +72,47 @@ export class CustomResource {
     }
 
     /**
-    * Adds a function to the CREATE queue
-    */
+     * Adds a function to the CREATE queue
+     */
     onCreate(func: func): this {
         this.createFunctions.push(func);
         return this;
     }
 
     /**
-    * Adds a function to the UPDATE queue
-    */
+     * Adds a function to the UPDATE queue
+     */
     onUpdate(func: func): this {
         this.updateFunctions.push(func);
         return this;
     }
 
     /**
-    * Adds a function to the DELETE queue
-    */
+     * Adds a function to the DELETE queue
+     */
     onDelete(func: func): this {
         this.deleteFunctions.push(func);
         return this;
     }
 
     /**
-    * Handles the Lambda event
-    */
+     * Handles the Lambda event
+     */
     handle(input?: any): this {
         const construct = this;
         try {
             let queue: func[];
-            if (this.event.RequestType == 'Create') queue = this.createFunctions;
-            else if (this.event.RequestType == 'Update') queue = this.updateFunctions;
-            else if (this.event.RequestType == 'Delete') queue = this.deleteFunctions;
+            if (this.event.RequestType == 'Create')
+                queue = this.createFunctions;
+            else if (this.event.RequestType == 'Update')
+                queue = this.updateFunctions;
+            else if (this.event.RequestType == 'Delete')
+                queue = this.deleteFunctions;
             else {
-                this.sendResponse('FAILED', `Unexpected request type: ${this.event.RequestType}`);
+                this.sendResponse(
+                    'FAILED',
+                    `Unexpected request type: ${this.event.RequestType}`
+                );
                 return this;
             }
 
@@ -112,13 +122,18 @@ export class CustomResource {
                 });
             }, Promise.resolve(input));
 
-            result.then(function (response) {
-                construct.logger.debug(response);
-                construct.sendResponse('SUCCESS', `${construct.event.RequestType} completed successfully`);
-            }).catch(function (err: AWS.AWSError) {
-                construct.logger.error(err, err.stack);
-                construct.sendResponse('FAILED', err.message || err.code);
-            });
+            result
+                .then(function (response) {
+                    construct.logger.debug(response);
+                    construct.sendResponse(
+                        'SUCCESS',
+                        `${construct.event.RequestType} completed successfully`
+                    );
+                })
+                .catch(function (err: AWS.AWSError) {
+                    construct.logger.error(err, err.stack);
+                    construct.sendResponse('FAILED', err.message || err.code);
+                });
         } catch (err) {
             construct.sendResponse('FAILED', err.message || err.code);
         }
@@ -126,24 +141,29 @@ export class CustomResource {
     }
 
     /**
-    * Sends CloudFormation response just before the Lambda times out
-    */
+     * Sends CloudFormation response just before the Lambda times out
+     */
     timeout() {
         const construct = this;
         const handler = () => {
             construct.logger.error('Timeout FAILURE!');
-            new Promise(() => construct.sendResponse('FAILED', 'Function timed out'))
-                .then(() => construct.callback(new Error('Function timed out')));
+            new Promise(() =>
+                construct.sendResponse('FAILED', 'Function timed out')
+            ).then(() => construct.callback(new Error('Function timed out')));
         };
         setTimeout(handler, this.context.getRemainingTimeInMillis() - 1000);
     }
 
     /**
-    * Sends CloudFormation response
-    */
+     * Sends CloudFormation response
+     */
     sendResponse(responseStatus: string, responseData: string) {
         const construct = this;
-        construct.logger.debug(`Sending response ${responseStatus}:\n${JSON.stringify(responseData)}`);
+        construct.logger.debug(
+            `Sending response ${responseStatus}:\n${JSON.stringify(
+                responseData
+            )}`
+        );
 
         var body = JSON.stringify({
             Status: responseStatus,
@@ -169,14 +189,16 @@ export class CustomResource {
             headers: {
                 'content-type': '',
                 'content-length': body.length,
-            }
+            },
         };
 
         construct.logger.info('SENDING RESPONSE...\n');
 
         var request = https.request(options, function (response: any) {
             construct.logger.debug(`STATUS: ${response.statusCode}`);
-            construct.logger.debug(`HEADERS: ${JSON.stringify(response.headers)}`);
+            construct.logger.debug(
+                `HEADERS: ${JSON.stringify(response.headers)}`
+            );
             construct.context.done();
         });
 
@@ -191,8 +213,8 @@ export class CustomResource {
 }
 
 /**
-* Logger class
-*/
+ * Logger class
+ */
 export interface Logger {
     log(message: any, ...optionalParams: any[]): void;
     info(message: any, ...optionalParams: any[]): void;
@@ -202,8 +224,8 @@ export interface Logger {
 }
 
 /**
-* LogLevels supported by the logger
-*/
+ * LogLevels supported by the logger
+ */
 export const enum LogLevel {
     ERROR,
     WARN,
@@ -212,15 +234,14 @@ export const enum LogLevel {
 }
 
 /**
-* Standard logger class
-*/
+ * Standard logger class
+ */
 export class StandardLogger {
-
     /**
-    * The log level
-    *
-    * @default LogLevel.WARN
-    */
+     * The log level
+     *
+     * @default LogLevel.WARN
+     */
     level: LogLevel;
 
     constructor(level?: LogLevel) {
@@ -228,40 +249,40 @@ export class StandardLogger {
     }
 
     /**
-    * Logs message with level ERROR
-    */
+     * Logs message with level ERROR
+     */
     error(message: any, ...optionalParams: any[]) {
         if (this.level < LogLevel.ERROR) return;
         console.error(message, ...optionalParams);
     }
 
     /**
-    * Logs message with level WARN
-    */
+     * Logs message with level WARN
+     */
     warn(message: any, ...optionalParams: any[]) {
         if (this.level < LogLevel.WARN) return;
         console.warn(message, ...optionalParams);
     }
 
     /**
-    * Logs message with level INFO
-    */
+     * Logs message with level INFO
+     */
     info(message: any, ...optionalParams: any[]) {
         if (this.level < LogLevel.INFO) return;
         console.info(message, ...optionalParams);
     }
 
     /**
-    * Logs message with level DEBUG
-    */
+     * Logs message with level DEBUG
+     */
     debug(message: any, ...optionalParams: any[]) {
         if (this.level < LogLevel.DEBUG) return;
         console.debug(message, ...optionalParams);
     }
 
     /**
-    * Alias for info
-    */
+     * Alias for info
+     */
     log(message: any, ...optionalParams: any[]) {
         this.info(message, ...optionalParams);
     }
